651d2d186ced4274858233007e94eb919c8f89f7
AnshShrivastava70 <anshshrivastava70@gmail.com>
2024-02-08 00:04:48 +0530

    Calculator App created

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default App;
 */
 
 
-        // ADDING Image 
+// ADDING Image 
 
 /*
  import logo from "./assets/logo.png";
@@ -50,7 +50,7 @@ export default App;
 
 // Adding Audio/Video in React 
 
-
+/*
 import React from "react";
 
 function App() {
@@ -89,4 +89,79 @@ return (
 
 export default App;
 
+*/
+
+
+// Building Calculator App
+
+import {
+  useState,
+  useRef
+} from "react";
+import "./Calc.css";
+
+function App() {
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
+
+  function plus(e) {
+    e.preventDefault();
+    setResult(result => result + Number(inputRef.current.value));
+  };
+
+  function minus(e) {
+    e.preventDefault();
+    setResult(result => result - Number(inputRef.current.value)); 
+  };
+
+  function times(e) {
+    e.preventDefault();
+    setResult(result => result  * Number(inputRef.current.value));
+
+  };
+
+  function divide(e) {
+    e.preventDefault();
+    setResult(result => result / Number(inputRef.current.value));
+  };
+
+  function resetInput(e) {
+    e.preventDefault();
+    inputRef.current.value = '';
+
+  };
+
+  function resetResult(e) {
+    e.preventDefault();
+    setResult(0);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p ref={resultRef}>
+          {result}
+        </p>
+        <input
+          pattern="[0-9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+        />
+        <button onClick={plus}>Add</button>
+        <button onClick={minus}>Subtract</button>
+        <button onClick={times}>Multiply</button>
+        <button onClick={divide}>Divide</button>
+        <button onClick={resetInput}>Reset Input</button>
+        <button onClick={resetResult}>Reset Result</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
 
